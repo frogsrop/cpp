e9d84ec0110c2ab13b0a6950036f0601c3f77f00
Ugay Yanis <ugai1yanis1@gmail.com>
2018-03-10 20:25:26 +0300

    swap update

--- DebugList/main.cpp
+++ DebugList/main.cpp
@@ -1,14 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <list>
-#include <memory>
-#include "debugList.h"
-
+#include<bits/stdc++.h>
+#include"debugList.h"
 using namespace std;
 
+debugList<int> x;
+debugList<int> y;
 
-int main()
-{
-    return 0;
+int main() {
+    int n = 10;
+    for(int i = 0;i<n;i++)
+    {
+        x.push_front(i);
+        y.push_front(n - 1 -i);
+    }
+    x.swap(y);
+    cout<<x.size_()<<' ';
+    cout<<y.size_()<<endl;
+    auto it = x.begin();
+    auto itt = y.begin();
+    for(int i = 0;i<n;i++)
+    {
+        cout<<(*it)<<' ';
+        cout<<(*itt)<<endl;
+        it++;
+        itt++;
+    }
+
+    debugList<int>::iterator_imp ittt;
+    ittt = it;
+    ittt--;
+    cout<<*ittt;
 }
-
